a4c7bdeb5c4c2f72f6428e9a66ad5072cec61dc8
Sudip Ghimire <ghimiresdp@gmail.com>
2023-04-04 06:39:26 +0545

    feat: CSV remove students

--- src/components/admin/StudentManager.tsx
+++ src/components/admin/StudentManager.tsx
@@ -6,8 +6,8 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Heading, Icon,
-  Input,
+  Heading, HStack, Icon,
+  Input, Spacer,
   Stack, Text, useToast,
 } from '@chakra-ui/react';
 import useTranslation from 'next-translate/useTranslation';
@@ -22,14 +22,18 @@ import { parse } from 'csv-parse';
 
 const LearnToEarnAddress = getContractAddress('LearnToEarn');
 
-const AddSingleStudentForm = () => {
+interface StudentManageProps {
+  action: 'add' | 'remove';
+}
+
+const ManageSingleStudent = (props: StudentManageProps) => {
 
   const { t } = useTranslation('admin');
   const [studentAddress, setStudentAddress] = useState<`0x${string}`>('0x');
 
   const { config, error: configError } = usePrepareContractWrite({
     address: LearnToEarnAddress,
-    functionName: 'addStudent',
+    functionName: props.action === 'add' ? 'addStudent' : 'removeStudent',
     args: [studentAddress],
     abi: LearnToEarnABI,
     enabled: isAddress(studentAddress),
@@ -43,31 +47,33 @@ const AddSingleStudentForm = () => {
     event.preventDefault();
     ContractWrite?.();
   }}>
+    <Heading fontSize={'lg'} mb={3}>{t(props.action == 'add' ? 'students.ADD' : 'students.REMOVE')}</Heading>
+
     <FormControl isInvalid={!(isAddress(studentAddress) || studentAddress == '0x')}>
       <FormLabel>
         {t('students.ENTER_ADDRESS')}
       </FormLabel>
-      <Input
-        fontFamily={'mono'}
-        value={studentAddress}
-        onChange={(event) => {
-          setStudentAddress(event.target.value as `0x${string}`);
-        }}
-      />
+      <HStack>
+        <Input
+          fontFamily={'mono'}
+          value={studentAddress}
+          onChange={(event) => {
+            setStudentAddress(event.target.value as `0x${string}`);
+          }}
+        />
+        <Button
+          type={'submit'} my={5} px={5} colorScheme={'blue'}
+          isLoading={contractWriteLoading}
+          isDisabled={(!!configError || !isAddress(studentAddress))}
+        >{t(props.action === 'add' ? 'students.ADD' : 'students.REMOVE')}</Button>
+      </HStack>
       <FormErrorMessage>{t('students.ENTER_VALID_ADDRESS')}</FormErrorMessage>
     </FormControl>
-    <Button
-      type={'submit'} my={5} colorScheme={'blue'}
-      isLoading={contractWriteLoading}
-      isDisabled={(!!configError || !isAddress(studentAddress))}
-    >{t('students.ADD_STUDENT')}</Button>
-    {/*{JSON.stringify(config)}*/}
-    <hr />
   </form>);
 };
 
 
-const AddMultipleStudentForm = () => {
+const AddMultipleStudentForm = (props: StudentManageProps) => {
   const { t } = useTranslation('admin');
   const toast = useToast();
   const [addresses, setAddresses] = useState<string[]>([]);
@@ -81,7 +87,7 @@ const AddMultipleStudentForm = () => {
 
   const { config, error: configError } = usePrepareContractWrite({
     address: LearnToEarnAddress,
-    functionName: 'addStudents',
+    functionName: props.action === 'add' ? 'addStudents' : 'removeStudents',
     args: [addresses],
     abi: LearnToEarnABI,
     enabled: !!addresses.length,
@@ -112,7 +118,8 @@ const AddMultipleStudentForm = () => {
           description: 'Could not find any wallet address in the first column of the CSV file.',
         });
       } else {
-        setAddresses(records);
+        // @ts-ignore
+        setAddresses([...new Set(records)]);
       }
       console.log(records);
     });
@@ -130,7 +137,7 @@ const AddMultipleStudentForm = () => {
   }, [acceptedFiles]);
 
   return (<>
-    <Heading fontSize={'lg'} mb={3}>Add Multiple Students</Heading>
+    <Heading fontSize={'lg'} mb={3}>{t(props.action == 'add' ? 'students.BULK_ADD' : 'students.BULK_REMOVE')}</Heading>
     <Flex
       {...getRootProps()}
       border={'dashed 3px'} borderColor={'blue.500'}
@@ -138,34 +145,48 @@ const AddMultipleStudentForm = () => {
       alignItems={'center'}
       justifyContent={'center'}
     >
-      {acceptedFiles.length ? <>
-        {acceptedFiles.map((file, index) =>
-          <Text key={index} p={3} borderWidth={'1px'} borderRadius={'lg'}>
-            <Icon as={FaFile} mr={2} />{file.name}
-          </Text>,
-        )}
+      {addresses.length ? <>
+        <Flex alignItems={'center'} justifyContent={'center'} p={5} flexWrap={'wrap'}>
+          {addresses.map((address) => (
+            <Badge
+              colorScheme={'green'} key={address} py={1} px={2} m={1} borderWidth={'1px'}
+              borderRadius={'full'} fontFamily={'mono'}
+            >
+              {address}
+            </Badge>
+          ))}
+        </Flex>
+        {/*{acceptedFiles.map((file, index) =>*/}
+        {/*  <Text key={index} p={3} borderWidth={'1px'} borderRadius={'lg'}>*/}
+        {/*    <Icon as={FaFile} mr={2} />{file.name}*/}
+        {/*  </Text>,*/}
+        {/*)}*/}
       </> : <>
         <Icon as={FaFile} mr={2} />
         <Text>Drag and drop CSV file, or click to select file</Text>
       </>}
       <input {...getInputProps()} />
     </Flex>
-    <Button
-      isDisabled={!addresses.length}
-      type={'submit'} my={5} colorScheme={'blue'} onClick={() => {
-      ContractWrite?.()
-    }}
-    >{t('students.ADD_STUDENT')}</Button>
-    <Box textAlign={'center'}>
-      {addresses.map((address) => (
-        <Badge
-          colorScheme={'green'} key={address} py={1} px={2} m={1} borderWidth={'1px'}
-          borderRadius={'full'} fontFamily={'mono'}
-        >
-          {address}
-        </Badge>
-      ))}
-    </Box>
+    <HStack>
+      <Spacer />
+      <Button
+        isLoading={contractWriteLoading}
+        colorScheme={'red'}
+        isDisabled={!addresses.length}
+        onClick={() => {
+          setAddresses([]);
+        }}>
+        {t('students.CLEAR_FORM')}
+      </Button>
+      <Button
+        isDisabled={!addresses.length || !!configError}
+        isLoading={contractWriteLoading}
+        type={'submit'} my={5} colorScheme={'blue'} onClick={() => {
+        ContractWrite?.();
+      }}
+      >{t(props.action === 'add' ? 'students.ADD' : 'students.REMOVE')}</Button>
+      <Spacer />
+    </HStack>
   </>);
 };
 
@@ -175,9 +196,12 @@ export const StudentManager = () => {
     <Container maxW={'container.lg'}>
       <Heading mb={5}>{t('students.HEADING')}</Heading>
       <Stack spacing={10}>
-        <AddSingleStudentForm />
-        <hr />
-        <AddMultipleStudentForm />
+        <ManageSingleStudent action={'add'} />
+        {/*<hr />*/}
+        <AddMultipleStudentForm action={'add'} />
+
+        <ManageSingleStudent action={'remove'} />
+        <AddMultipleStudentForm action={'remove'} />
       </Stack>
     </Container>
   </>);
