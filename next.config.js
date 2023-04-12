bf5c990dceceaf09e4e85b9c8946314c484043a5
hourglasshoro <hourglasshoro.628@gmail.com>
2023-04-12 10:13:30 +0900

    add: safe connector

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const nextTranslate = require('next-translate-plugin');
 module.exports = nextTranslate({
   reactStrictMode: true,
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://app.safe.global' },
+        ],
+      },
+    ];
+  },
 });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { WagmiConfig, createClient, configureChains } from 'wagmi'
+import {WagmiConfig, createClient, configureChains, useConnect} from 'wagmi'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from '@/layouts/theme'
@@ -6,6 +6,9 @@ import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import Layout from '@/layouts/Layout'
 import { publicProvider } from '@wagmi/core/providers/public'
 import { defaultChain } from '@/utils/contract';
+import {SafeConnector} from "@wagmi/connectors/safe";
+import {FC, useEffect} from "react";
+import {NextComponentType, NextPageContext} from "next";
 
 const { chains, provider } = configureChains(
   [defaultChain],
@@ -13,21 +16,48 @@ const { chains, provider } = configureChains(
 )
 
 const client = createClient({
-  autoConnect: true,
-  connectors: [new MetaMaskConnector({ chains })],
+  autoConnect: false,
+  connectors: [new MetaMaskConnector({ chains }), new SafeConnector({chains,  options: {
+      allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
+      debug: true,
+    }})],
   provider,
 })
 
+const AUTOCONNECTED_CONNECTOR_IDS = ['safe'];
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+
   return (
     <WagmiConfig client={client}>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
+        <Inner Component={Component} pageProps={pageProps}/>
     </WagmiConfig>
   )
+}
+
+const Inner:FC<{
+    Component: NextComponentType<NextPageContext, any, {}>
+    pageProps: any
+}> = ({ Component, pageProps })=>{
+    const { connect, connectors } = useConnect();
+
+    useEffect(() => {
+        AUTOCONNECTED_CONNECTOR_IDS.forEach((connector) => {
+            const connectorInstance = connectors.find((c) => c.id === connector && c.ready);
+
+            if (connectorInstance) {
+                connect({ connector: connectorInstance });
+            }
+        });
+    }, [connect, connectors]);
+
+    return(
+        <ChakraProvider theme={theme}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ChakraProvider>
+    )
 }
 
 export default MyApp
