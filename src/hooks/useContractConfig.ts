8e6949b502b23de53e5b1d7710391622a851fe79
Sudip Ghimire <ghimiresdp@gmail.com>
2025-01-27 13:05:51 +0000

    feat: add FaucetABI in `useContractConfig` hook.

--- src/hooks/useContractConfig.ts
+++ src/hooks/useContractConfig.ts
@@ -1,20 +1,24 @@
 import { getContractAddress } from '@/utils/contract';
 import LearnToEarnABI from '@/utils/abis/LearnToEarn.json';
 import CitNFTABI from '@/utils/abis/CitNFT.json';
-import CitCoinABI from '@/utils/abis/CitCoin.json';
+import cjpyABI from '@/utils/abis/CJPY.json';
+import faucetABI from '@/utils/abis/Faucet.json';
 
-export const UseContractConfig = (name: 'CitCoin' | 'LearnToEarn' | 'NFT') => {
+export const UseContractConfig = (name: 'cJPY' | 'LearnToEarn' | 'NFT' | 'Faucet') => {
   const contractAddress = getContractAddress(name);
   let abi: any;
   switch (name) {
     case 'LearnToEarn':
       abi = LearnToEarnABI;
       break;
-    case 'CitCoin':
-      abi = CitCoinABI;
+    case 'cJPY':
+      abi = cjpyABI;
       break;
     case 'NFT':
       abi = CitNFTABI;
+      break;
+    case 'Faucet':
+      abi = faucetABI;
       break;
     default:
       throw 'Invalid Contract Name selected';
